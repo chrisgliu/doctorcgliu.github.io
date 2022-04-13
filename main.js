2241cad9c5387cbfd3d4df422239409c9a5b6a3b
cgliu-create <chrisliu2438@gmail.com>
2022-04-13 04:17:12 -0400

    s

---
+++ main.js
@@ -0,0 +1,24 @@
+
+ function tbm() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    var images = document.body.getElementsByTagName("g");
+    for (var image of images) {
+        image.classList.toggle("dark-img");
+    }
+    var circles = document.body.getElementsByTagName("circle");
+    for (var circle of circles) {
+        circle.classList.toggle("dark-img");
+    }
+    var table = document.body.getElementsByTagName("td");
+    for (var entry of table) {
+        entry.classList.toggle("dark-img");
+    }
+}
+
+const navIcon = document.querySelector(".nav-icon");
+const nav = document.querySelector("nav");
+
+navIcon.onclick = function () {
+    nav.classList.toggle('show');
+}
